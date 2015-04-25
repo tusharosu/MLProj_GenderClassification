525ef669c8e18567895a388e7fd0d7763fb256e5
singh-shashank <shashanksingh14@gmail.com>
2015-04-25 02:58:18 -0400

    removed project folder

--- project/RemoveRetweets.py
+++
@@ -1,5 +0,0 @@
-wordFile = open('myDataModified.txt', 'w')
-with open('myData.txt') as f:
-    for line in f:
-        if not'RT' in line:
-            wordFile.write(line)
--- project/TwittHandleExtract.py
+++
@@ -1,6 +0,0 @@
-wordFile = open('TweetsTemp.txt', 'w')
-count=0
-with open('TempList.txt') as f:
-    for line in f:
-        if '@' in line:
-            wordFile.write(line+'\n')
